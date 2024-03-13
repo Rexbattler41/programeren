ddefbfe13a4821623726c6e2475b6bf39e46cbdb
Rex <113677847+Rexbattler41@users.noreply.github.com>
2024-03-13 14:11:06 +0100

    winnen toegevoegd

--- wedstrijdteller/wedstrijdteller.js
+++ wedstrijdteller/wedstrijdteller.js
@@ -1,17 +1,31 @@
 console.log('hallo wereld');
 
+let team1Points = 0;
+let team2Points = 0;
+
+
+
 function pointTeam1(event){
-    let team1Points = counterTeam1.outerText;
-    counterTeam1.innerText = parseInt(team1Points) + 1;
+    team1Points++;
+    if ((team1Points > 25|| team2Points > 25) && Math.abs(team1Points - team2Points) >= 2) {
+        team1Points = 0;
+        team2Points = 0;
+        alert('gewonnen!')
+        counterTeam2.innerText = team2Points;
+    }
+    counterTeam1.innerText = team1Points;
 }
 
 function pointTeam2(event){
-    let team2Points = counterTeam2.outerText;
-    counterTeam2.innerText = parseInt(team2Points) + 1;
+    team2Points++;
+    if ((team1Points > 25|| team2Points > 25) && Math.abs(team2Points - team1Points) >= 2) {
+        team1Points = 0;
+        team2Points = 0;
+        alert('gewonnen!')
+        counterTeam1.innerText = team1Points;
+    }
+    counterTeam2.innerText = team2Points;
 }
-
-counterTeam1.addEventListener('click', pointTeam1);
-counterTeam2.addEventListener('click', pointTeam2);
 
 function handleStartButton(event){
     console.dir(servingTeam1);
@@ -32,12 +46,14 @@ function handleStartButton(event){
             alert("selecteer wie begint")
         } else {
             console.log("team 1 begint")
-            input_teams.remove()
+            input_teams.style.display = 'none'
         }
     } else {
         console.log("team 2 begint")
-        input_teams.remove()
+        input_teams.style.display = 'none'
     }
+    counterTeam1.addEventListener('click', pointTeam1);
+    counterTeam2.addEventListener('click', pointTeam2);
 };
 
 startButton.addEventListener('click', handleStartButton);
